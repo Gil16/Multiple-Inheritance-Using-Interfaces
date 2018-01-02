9d9ea61f6e5a2593e6985d68cbd3c77ab6ffe7e5
Gil16 <gilbli166@gmail.com>
2018-01-02 17:08:38 +0200

    Part B is done - need to test it

--- src/OOP/Solution/OOPMultipleControl.java
+++ src/OOP/Solution/OOPMultipleControl.java
@@ -5,6 +5,7 @@ import OOP.Provided.*;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static OOP.Provided.OOPInaccessibleMethod.*;
 
@@ -26,12 +27,14 @@ public class OOPMultipleControl {
         List<Class<?>> interfacesList = new LinkedList<Class<?>>();
         Set<Class<?>> duplicateSet = new HashSet<Class<?>>();
         getAllInterfacesInTheGraph(interfaceClass, interfacesList, duplicateSet);
+
         // Checking if all the interfaces has OOPMultipleInterface annotation
         for (Class<?> anInterface : interfacesList){
             if(!anInterface.isAnnotationPresent(OOPMultipleInterface.class)) {
                 throw new OOPBadClass(anInterface);
             }
         }
+
         // Checking if there is illegal inner calling in the graph
         List<ForbiddenAccess> badMethods = new LinkedList<ForbiddenAccess>();
         for (Class<?> anInterface : interfacesList) {
@@ -39,22 +42,24 @@ public class OOPMultipleControl {
                 if (!(aMethod.isAnnotationPresent(OOPMultipleMethod.class))) {
                     throw new OOPBadClass(aMethod);
                 }
-                Class<?> Callee = aMethod.getAnnotation(OOPInnerMethodCall.class).callee();
-                Class<?> Caller = aMethod.getAnnotation(OOPInnerMethodCall.class).caller();
-                String MethodName = aMethod.getAnnotation(OOPInnerMethodCall.class).methodName();
-                Class<?>[] ArgTypes = aMethod.getAnnotation(OOPInnerMethodCall.class).argTypes();
-                Method calledMethod = null;
-                try {
-                    calledMethod = Callee.getDeclaredMethod(MethodName, ArgTypes);
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();        // Should never get here!! if it does, it's your problem!
-                }
-                ForbiddenAccess badAccess = new ForbiddenAccess(Caller, Callee, aMethod);
-                if (calledMethod.getAnnotation(OOPMultipleMethod.class).modifier() == OOPMethodModifier.PRIVATE) {
-                    badMethods.add(badAccess);
-                } else if (calledMethod.getAnnotation(OOPMultipleMethod.class).modifier() == OOPMethodModifier.PROTECTED) {
-                    if (!checkIfInterface1IsInInterface2Hierarchy(Callee, Caller)) {
+                if(aMethod.isAnnotationPresent(OOPInnerMethodCall.class)) {
+                    Class<?> Callee = aMethod.getAnnotation(OOPInnerMethodCall.class).callee();
+                    Class<?> Caller = aMethod.getAnnotation(OOPInnerMethodCall.class).caller();
+                    String MethodName = aMethod.getAnnotation(OOPInnerMethodCall.class).methodName();
+                    Class<?>[] ArgTypes = aMethod.getAnnotation(OOPInnerMethodCall.class).argTypes();
+                    Method calledMethod = null;
+                    try {
+                        calledMethod = Callee.getDeclaredMethod(MethodName, ArgTypes);
+                    } catch (NoSuchMethodException e) {
+                                // Should never get here!! if it does, it's your problem!
+                    }
+                    ForbiddenAccess badAccess = new ForbiddenAccess(Caller, Callee, aMethod);
+                    if (calledMethod.getAnnotation(OOPMultipleMethod.class).modifier() == OOPMethodModifier.PRIVATE) {
                         badMethods.add(badAccess);
+                    } else if (calledMethod.getAnnotation(OOPMultipleMethod.class).modifier() == OOPMethodModifier.PROTECTED) {
+                        if (!checkIfInterface1IsInInterface2Hierarchy(Callee, Caller)) {
+                            badMethods.add(badAccess);
+                        }
                     }
                 }
             }
@@ -65,21 +70,41 @@ public class OOPMultipleControl {
 
         // There are no inheritance ambiguities in the graph
         if(duplicateSet.size() > 0){
-
-        }
-
-
-        for(Class<?> anInterface : interfacesList){
-            for(Method aMethod : anInterface.getDeclaredMethods()){
-                if(aMethod.isAnnotationPresent(OOPInnerMethodCall.class)){
-                    if(duplicateInterfaces.contains(aMethod.getAnnotation(OOPInnerMethodCall.class).callee())){
-                        throw new OOPInherentAmbiguity(anInterface,
-                                getFirstDefinedMethodInGraph(aMethod.getAnnotation(OOPInnerMethodCall.class).callee(),
-                                            aMethod),aMethod);
+            // get all the duplicates in the graph (even their "fathers")
+            List<Class<?>> tempList = duplicateSet.stream().collect(Collectors.toList());
+            for(Class<?> anInterface : tempList){
+                Class<?> interfaceIterator = anInterface;
+                while(interfaceIterator.getInterfaces().length > 0){
+                    duplicateSet.add(interfaceIterator.getInterfaces()[0]); // get the interface "father"
+                    interfaceIterator = interfaceIterator.getInterfaces()[0];
+                }
+            }
+            for(Class<?> anInterface : interfacesList){
+                for(Method aMethod : anInterface.getDeclaredMethods()){
+                    if(aMethod.isAnnotationPresent(OOPInnerMethodCall.class)){
+                        if(duplicateSet.contains(aMethod.getAnnotation(OOPInnerMethodCall.class).callee())){
+                            Class<?> highestInterface = aMethod.getAnnotation(OOPInnerMethodCall.class).callee();
+                            Class<?> interfaceIterator = highestInterface;
+                            while(interfaceIterator.getInterfaces().length > 0){
+                                try{
+                                    interfaceIterator.getInterfaces()[0].getDeclaredMethod(aMethod.
+                                                    getAnnotation(OOPInnerMethodCall.class).methodName(),
+                                            aMethod.getAnnotation(OOPInnerMethodCall.class).argTypes());
+                                    highestInterface = interfaceIterator.getInterfaces()[0];
+                                }catch(NoSuchMethodException e){
+                                    // continue
+                                }
+                                interfaceIterator = interfaceIterator.getInterfaces()[0];
+                            }
+                            throw new OOPInherentAmbiguity(interfaceClass, highestInterface,aMethod);
+                        }
                     }
                 }
             }
+
         }
+
+
 
     }
 
