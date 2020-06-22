53cfa2d5282a9d7fb4bdfaff45753774ad109cb9
Gil16 <gilbli166@gmail.com>
2020-06-22 10:23:27 +0300

    Refactor

--- OOP/Solution/OOPMultipleMethod.java
+++ OOP/Solution/OOPMultipleMethod.java
@@ -1,13 +1,11 @@
 package OOP.Solution;
 
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static OOP.Solution.OOPMethodModifier.PUBLIC;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 
 @Target(ElementType.METHOD)
 @Retention(RUNTIME)
